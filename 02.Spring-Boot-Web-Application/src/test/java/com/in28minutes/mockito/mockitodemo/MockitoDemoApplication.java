1a66f013546ae88c082d3bea8ba7339620e3a753
haoch <haoch@DESKTOP-0JM2N73>
2019-06-26 19:34:29 -0500

    add 05_spring_boot_advanced

---
+++ 02.Spring-Boot-Web-Application/src/test/java/com/in28minutes/mockito/mockitodemo/MockitoDemoApplication.java
@@ -0,0 +1,10 @@
+package com.in28minutes.mockito.mockitodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class MockitoDemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoDemoApplication.class, args);
+	}
+}
---
+++ 05_spring-boot-advanced/src/main/java/com/in28minutes/springboot/spring_boot_advanced/App.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.spring_boot_advanced;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
