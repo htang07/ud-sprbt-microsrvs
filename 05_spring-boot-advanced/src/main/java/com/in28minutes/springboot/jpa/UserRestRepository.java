ae910a64563d216a0d8f7bbabae7ba3e943d6f70
haoch <haoch@DESKTOP-QKM1BR5>
2019-07-07 03:36:42 -0500

    add jpa

--- 05_spring-boot-advanced/src/main/java/com/in28minutes/springboot/jpa/UserRestRepository.java
+++ 05_spring-boot-advanced/src/main/java/com/in28minutes/springboot/jpa/UserRestRepository.java
@@ -2,6 +2,7 @@ package com.in28minutes.springboot.jpa;
 
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -9,5 +10,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(path = "users", collectionResourceRel = "users")
 public interface UserRestRepository extends
 		PagingAndSortingRepository<User, Long> {
-	List<User> findByRole(@Param("role") String role);
+	List<User> findByRole(@Param("role") String role, Sort sort);
+//	List<User> findByRole( String role);
 }
